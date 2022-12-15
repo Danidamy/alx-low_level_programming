d339be9654b35c4f19ac81c00c6bc9537eeec525
“Danidamy” <dhanidami@gmail.com>
2022-12-15 05:53:21 -0800

    checking if an uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the parameter is an upper case character
+ * @c: input character
+ * Return: 1 if it is an upper case and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 95)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
